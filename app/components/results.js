a8b8a80b5df67be547e2a37821b7449a9be015f7
Kyle Burke <kyleburke@Kyle-Burkes-MacBook-Pro.local>
2019-07-19 17:01:40 -0500

    save

---
+++ app/components/results.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { battle } from '../utils/api'
+
+export default class Results extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			winner: null,
+			loser: null,
+			error: null,
+			loading: true
+		}
+	}
+
+	componentDidMount() {
+		const { playerOne, playerTwo } = this.props;
+
+		battle([playerOne, playerTwo])
+			.then((players) => {
+				this.setState({
+					winner: players[0],
+					loser: players[1],
+					error: null,
+					loading: false
+				})
+			})
+			.catch(({message}) => {
+				this.setState({
+					error: message,
+					loading: false
+				})
+			})
+	}
+
+	render() {
+		const {winner, loser, error, loading} = this.state;
+
+		if(loading === true) {
+			return <p>LOADING</p>
+		}
+
+		if(error === true) {
+			return (
+				<p className='center-text error'>
+					{error}
+				</p>
+			)
+		}
+
+		return(
+			<div className='grid space-around container-sm '>
+				<div className='card bg-ligth'></div>
+				<div className='card bg-light'></div>
+			</div>
+		)
+	}
+}
